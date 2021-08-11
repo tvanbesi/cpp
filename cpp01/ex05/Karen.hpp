93cb398ae19c8c5fdeed291d52d7726c01d28b12
tvanbesi <tvanbesi@student.42.fr>
2021-08-11 17:36:30 +0200

    ex05

---
+++ cpp01/ex05/Karen.hpp
@@ -0,0 +1,23 @@
+#ifndef KAREN_HPP
+# define KAREN_HPP
+
+#include <string>
+
+class Karen {
+
+public:
+
+	Karen(void);
+	~Karen(void);
+	void	complain(std::string level);
+
+private:
+
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+
+};
+
+#endif
